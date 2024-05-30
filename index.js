d3816e7554664da29538575fa91a22771c99296a
asanibrahim <asanib98@gmail.com>
2024-05-30 23:58:02 +0530

    add output

--- index.js
+++ index.js
@@ -1,30 +1,18 @@
-// You are given an array prices where prices[i] is the price of a given stock on the ith day.
-// You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
-// Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
+// Given an integer x, return true if x is a palindrome,and false otherwise.
 
-let prices = [7, 1, 5, 3, 6, 4];
+let input = -121;
 
-let prices2 = [7, 6, 4, 3, 1];
+function isPalindrome(str){
+ let convertString = str.toString()
+ let convertWord = convertString.split(' ').reverse().join('')
+ return convertString === convertWord
+}
+ 
+console.log(isPalindrome(input));
 
-function maxProfitOfStock(price) {
-  let left = 0;
-  let right = 1;
-  let maxProfit = 0;
 
-  while (right < price.length) {
-    if (price[left] < price[right]) {
-      let profit = price[right] - price[left];
-      maxProfit = Math.max(maxProfit, profit);
-    } else {
-      left = right;
-    }
-    right++;
-  }
-  return maxProfit;
+function isPalindrome2(x){
+  return x < 0 ? false: x=== +x.toString().split('').reverse().join('')
 }
 
-console.log(maxProfitOfStock(prices));
-console.log(maxProfitOfStock(prices2));
-
-//output 5
-//output 0
+console.log(isPalindrome2(input));
