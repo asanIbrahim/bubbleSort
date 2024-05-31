551797fbd301ce5bad8a288ae433631fbb021af7
asanibrahim <asanib98@gmail.com>
2024-06-01 01:38:36 +0530

    add value

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
-// Given an integer x, return true if x is a palindrome,and false otherwise.
+const value = [5,2,3,1,15,21,12,9,8,6,11,7]
 
-let input = 10;
 
-function isPalindrome(str){
- let convertString = str.toString()
- let convertWord = convertString.split('').reverse().join('')
- return convertString === convertWord
+function bubbleSort(str){
+
+  for(i =0 ; i < str.length; i++){
+    for(j=0 ; j< str.length ; j++){
+      if(str[j] > str[j+1]){
+        let tem = str[j]
+        str[j] = str[j+1]
+        str[j+1] = tem
+      }
+    }
+  }
+
+  return str
 }
- 
-console.log(isPalindrome(input));
 
-
-function isPalindrome2(x){
-  return x < 0 ? false: x=== +x.toString().split('').reverse().join('')
-}
-
-console.log(isPalindrome2(input));
+console.log(bubbleSort(value));
