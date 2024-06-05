b119bcb5998daec2ca078ca9da3bf21f6ff96d9f
asanibrahim <asanib98@gmail.com>
2024-06-05 23:28:40 +0530

    add roated array

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function roateArray(num,k){
 }
 
 console.log(roateArray(water,k));
- ////[1, 2, 3, 6, 5, 4] output for roateArray
+ /////[1, 2, 3, 6, 5, 4] output for roateArray
 //// [3, 99, -1, -100] output
 ////console.log(roateArray(value,k));//
 //-output [5, 6, 7, 1, 2, 3, 4]//
