dd53ce1dcee79b70d18d719d06a0cd1236210eb1
asanibrahim <asanib98@gmail.com>
2024-06-17 05:58:33 +0530

    add

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ let nums = [-1,-100,3,99]
 let water = [6,5,4,1,2,3]
 let k =3;
 
-const roateArray = (num,k) => {
+function roateArray (num,k) {
   k = k % num.length
  num.unshift(...num.splice(num.length -k ,k))
  return num
